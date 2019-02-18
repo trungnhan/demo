b4164983e2fbd0c9d11b1b1d7241797b2bbebb8f
nhan.huynh <nhan.huynh@titancorpvn.com>
2019-02-18 14:09:26 +0700

    edit source

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import '../public/style.scss'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { HotTable } from '@handsontable-pro/react'
-import data from '../public/data.json'
-
+import BlockBanner from './blockBanner.js'
+import data from './data.json'
 class App extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.data = data
   }
-
   render() {
     return (
-      <div id="hot-app" className='my-container'>
-        <HotTable data={this.data} colHeaders={true} rowHeaders={true} width="600" height="300" stretchH="all" />
+      <div className='my-container'>
+        <div className='block-banners'>
+          {this.data.map((item, index) => <BlockBanner key={index} banner={item}/>)}
+        </div>
       </div>
-    );
+    )
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: {
     main: [
-      './src/adv.js'
+      './src/index.js'
     ]
   },
   watch: true,
